0528424b10a6d3620ecaf26c8672ef5df739a796
Vladimirx16 <vova08090@mail.ru>
2019-03-09 13:46:49 +0300

    OOP

--- src/lesson_4/polymorphism/Polymorphism.java
+++ src/lesson_4/polymorphism/Polymorphism.java
@@ -1,9 +1,20 @@
 package lesson_4.polymorphism;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Polymorphism {
     public static void main(String[] args) {
-        
+        ArrayList figures = new ArrayList();
+        figures.add(new Point());
+        figures.add(new LineSegment());
+        figures.add(new Triangle());
+
+        for (int i = 0; i < figures.size(); i++){
+            Point figure = (Point) figures.get(i);
+            System.out.println(figure.getDescription());
+            System.out.println("Площадь: " + figure.getSquare());
+            System.out.println("Периметр: " + figure.getPerimeter());
+        }
     }
 }
